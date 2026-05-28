264015e9e03ae2a38b62963e15f0f60810671a2b
LuguGMD <lugo.bhz@gmail.com>
2026-05-28 16:31:59 -0300

    Feat: Ataque Buzina

--- Assets/Scripts/Combat/Entities/Characters/BuzinaCombatController.cs
+++ Assets/Scripts/Combat/Entities/Characters/BuzinaCombatController.cs
@@ -1,14 +1,15 @@
+using RPG.Combat.Actions;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class BuzinaCombatController : CharacterController
     {
-        //[SerializeField] private DonLiponWeightAttack _weightAttack;
+        [SerializeField] private BuzinaDriveAttack _driveAttack;
 
         protected override void InitCombatActions()
         {
-            //_actions.Add(_weightAttack);
+            _actions.Add(_driveAttack);
 
             base.InitCombatActions();
         }
