426bd0adc6a0746fa83c9daacb99c3e114aae77a
LuguGMD <lugo.bhz@gmail.com>
2026-06-19 12:59:24 -0300

    Feat: Sistema de Save alterado

--- Assets/Scripts/Save/SaveAdapter.cs
+++ Assets/Scripts/Save/SaveAdapter.cs
@@ -4,19 +4,7 @@ namespace RPG.Save
 {
     public abstract class SaveAdapter<T1, T2>
     {
-        private T1 _class;
-
-        public SaveAdapter(T1 classSave)
-        {
-            _class = classSave;
-        }
-
-        #region Properties
-
-        public T1 ClassSave => _class;
-
-        #endregion
-
         public abstract void DataToClass(T1 classSave, T2 dataSave);
+        public abstract void ClassToData(T1 classSave, T2 dataSave);
     }
 }
