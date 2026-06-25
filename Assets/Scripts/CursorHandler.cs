7fea86217414f4353f22debc4624a2c7d4b4e6f7
LuguGMD <lugo.bhz@gmail.com>
2026-06-25 18:24:27 -0300

    Style: Cursor & Painel Upgrade

---
+++ Assets/Scripts/CursorHandler.cs
@@ -0,0 +1,10 @@
+using Lugu.Singleton;
+using UnityEngine;
+
+namespace RPG
+{
+    public class CursorHandler : SingletonMonoPersistent<CursorHandler>
+    {
+        //private readonly static Vector2 HOTSPOT = new Vector2(0, 0f);
+    }
+}
