e4107956f418285cdde045cae9a73c9220b491f1
LuguGMD <lugo.bhz@gmail.com>
2026-06-27 18:34:50 -0300

    Style: Cursor Novo

--- Assets/Scripts/CursorHandler.cs
+++ Assets/Scripts/CursorHandler.cs
@@ -1,10 +1,34 @@
 using Lugu.Singleton;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace RPG
 {
     public class CursorHandler : SingletonMonoPersistent<CursorHandler>
     {
-        //private readonly static Vector2 HOTSPOT = new Vector2(0, 0f);
+        [SerializeField] private Texture2D[] _cursor;
+        [SerializeField] private Texture2D[] _cursorClick;
+        private Texture2D _currentCursorTexture;
+
+        private void Update()
+        {
+            //TO DO adicioanar verificacao de hover para mudar o index
+            int cursorIndex = 0;
+            Texture2D currentCursorTexture = _cursor[cursorIndex];
+
+            if (Mouse.current != null)
+            {
+                if (Mouse.current.leftButton.isPressed)
+                {
+                    currentCursorTexture = _cursorClick[cursorIndex];
+                }
+            }
+
+            if (_currentCursorTexture != currentCursorTexture)
+            {
+                _currentCursorTexture = currentCursorTexture;
+                Cursor.SetCursor(_currentCursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+            }
+        }
     }
 }
