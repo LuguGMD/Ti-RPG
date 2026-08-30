bf64a4558f0eaabda869c2c67ade175cc220b9fc
lucca100S <luccacatalan@gmail.com>
2026-08-30 16:50:54 -0300

    Feat: VFX de Cura ativado com Super do Apresentador

--- Assets/Scripts/Combat/VFX/CombatVFXManager.cs
+++ Assets/Scripts/Combat/VFX/CombatVFXManager.cs
@@ -13,6 +13,7 @@ namespace RPG.Combat.VFX
             ActionsManager.Instance.OnStageEntityCreated += PlaySpawnVFX;
             ActionsManager.Instance.OnStageEntityDefeated += PlaySpawnVFX;
             ActionsManager.Instance.OnCharacterHealed += PlayHealVFX;
+            ActionsManager.Instance.OnApresentadorHealed += PlayHealVFXApresentador;
         }
 
         void OnDisable()
@@ -29,6 +30,11 @@ namespace RPG.Combat.VFX
         void PlayHealVFX(CharacterController entity)
         {
             Instantiate(healVFX, entity.transform.position, Quaternion.identity);
+        }
+
+        void PlayHealVFXApresentador()
+        {
+            Instantiate(healVFX, CombatManager.Apresentador.transform.position, Quaternion.identity);
         }
     }
 }
